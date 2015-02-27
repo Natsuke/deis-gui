a7fca291fdd987c5b10535d4540b2d959b34174c
Andy Vanbutsele <andy.vanbutsele@gmail.com>
2015-02-27 12:41:27 +0100

    add: scaling an app function with templating to show each container details

--- app/scripts/components/dashboard/dashboardController.js
+++ app/scripts/components/dashboard/dashboardController.js
@@ -7,4 +7,21 @@ angular.module('deis-gui')
       LoginService.logout();
     };
 
+    $scope.listApps = function() {
+      $scope.apps = DeisRestangular.apps.getList().$object;
+    };
+
+    $scope.appContDetail = function(id) {
+      $scope.id = id;
+      $scope.appCont = DeisRestangular.one('apps', id).one('containers').get().$object;
+    };
+
+    $scope.scaleApp = function(id, number) {
+      var scaling = {
+        "web": number
+      };
+      DeisRestangular.one('apps', id).post("scale/", scaling);
+      $scope.id = 0;
+    };
+
   });
